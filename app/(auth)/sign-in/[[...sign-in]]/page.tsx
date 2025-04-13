b6e9f785868d6c65b92278218f71f8202f7c2355
Ankit Verma <sfdcankitverma@gmail.com>
2025-04-13 23:04:54 +0530

    fixed sign in page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,13 +3,22 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <><div className='grid grid-cols-1 md:grid-cols-2'>
-      <div>
-        <Image src={'/login.png'} alt='login'
-          width={700} height={1000} className='w-full' />
-      </div>
-    </div><div className='flex justify-center items-center h-screen order-first md:order-last'>
-        <SignIn />
-      </div></>
+    <><div className='grid grid-cols-1 md:grid-cols-2 min-h-screen'>
+    {/* Left Side - Image */}
+    <div className='flex justify-center items-center'>
+      <Image 
+        src='/login.png' 
+        alt='login' 
+        width={500} 
+        height={700} 
+        className='w-full h-auto object-contain' 
+      />
+    </div>
+
+    {/* Right Side - Sign In */}
+    <div className='flex justify-center items-center'>
+      <SignIn />
+    </div>
+  </div></>
   )
 }
